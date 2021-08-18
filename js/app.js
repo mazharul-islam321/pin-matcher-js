ad7250b608dca44c5c84bcb78f432c8e5716bcd5
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-08-18 20:53:40 +0600

    create valid pin number function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-
+// get valid pin number
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + "";
+    if (pinString.length == 4) {
+        return pin;
+    } else {
+        // console.log("got three digit string and calling again", pin);
+        return getPin();
+    }
+}
+// generate pin function
+function generatePin() {
+    const pin = getPin();
+    document.getElementById("display-pin").value = pin;
+}
